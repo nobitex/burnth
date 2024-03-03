980bc4a175a1fcfb32e371f025247d89ca1022f5
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2024-03-03 17:46:45 +0330

    Move main script to root

--- zk/spend.py
+++ zk/spend.py
@@ -2,7 +2,7 @@ import json
 import os
 import io
 import ast
-from field import Field
+from .field import Field
 
 
 def get_spend_proof(salt, balance, withdrawnBalance):
@@ -16,16 +16,14 @@ def get_spend_proof(salt, balance, withdrawnBalance):
             f,
         )
 
-    os.system(
-        "cd zk && make gen_spend_witness && make gen_spend_proof"
-    )
+    os.system("cd zk && make gen_spend_witness && make gen_spend_proof")
 
     proof = open("/tmp/spend_proof.json", "r").read()
-    proof = ast.literal_eval(proof)    
+    proof = ast.literal_eval(proof)
     proof = [
         [Field(int(s, 16)).val for s in proof[0]],
         [[Field(int(s, 16)).val for s in p] for p in proof[1]],
         [Field(int(s, 16)).val for s in proof[2]],
     ]
-        
+
     return proof
