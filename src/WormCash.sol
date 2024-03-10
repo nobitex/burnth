4427e424c791b9130fcf5f5dcc70e48653430e24
Stephen Hawking <amirh.eth@gmail.com>
2024-03-10 13:09:22 +0330

    refactor(wormcash): use msg.sender without cache in local variable

--- src/WormCash.sol
+++ src/WormCash.sol
@@ -5,7 +5,6 @@ import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 contract WormCash is ERC20 {
     IERC20 burnth_contract;
     uint256 starting_block;
-    address sender = msg.sender;
 
     mapping(uint256 => uint256) public epoch_totals;
     mapping(uint256 => mapping(address => uint256)) public epochs;
@@ -23,14 +22,17 @@ contract WormCash is ERC20 {
         return (block.number - starting_block) / BLOCK_PER_EPOCH;
     }
 
-    function approximate(uint256 amount_per_epoch, uint256 num_epochs) public view returns (uint256) {
+    function approximate(
+        uint256 amount_per_epoch,
+        uint256 num_epochs
+    ) public view returns (uint256) {
         uint256 mint_amount = 0;
         uint256 currEpoch = currentEpoch();
         for (uint256 i = 0; i < num_epochs; i++) {
             uint256 epochIndex = currEpoch + i;
-            uint256 user = epochs[epochIndex][sender] + amount_per_epoch;
+            uint256 user = epochs[epochIndex][msg.sender] + amount_per_epoch;
             uint256 total = epoch_totals[epochIndex] + amount_per_epoch;
-            mint_amount += rewardOf(epochIndex) * user / total;
+            mint_amount += (rewardOf(epochIndex) * user) / total;
         }
         return mint_amount;
     }
@@ -43,26 +45,36 @@ contract WormCash is ERC20 {
         return reward;
     }
 
-    function participate(uint256 amount_per_epoch, uint256 num_epochs) external {
+    function participate(
+        uint256 amount_per_epoch,
+        uint256 num_epochs
+    ) external {
         uint256 currEpoch = currentEpoch();
         for (uint256 i = 0; i < num_epochs; i++) {
             epoch_totals[currEpoch + i] += amount_per_epoch;
-            epochs[currEpoch + i][sender] += amount_per_epoch;
+            epochs[currEpoch + i][msg.sender] += amount_per_epoch;
         }
-        burnth_contract.transferFrom(sender, address(this), num_epochs * amount_per_epoch); // TODO: Handle exception
+        burnth_contract.transferFrom(
+            msg.sender,
+            address(this),
+            num_epochs * amount_per_epoch
+        ); // TODO: Handle exception
     }
 
     function claim(uint256 starting_epoch, uint256 num_epochs) external {
-        require(starting_epoch + num_epochs <= currentEpoch(), "Cannot claim an ongoing epoch!");
+        require(
+            starting_epoch + num_epochs <= currentEpoch(),
+            "Cannot claim an ongoing epoch!"
+        );
         uint256 mint_amount = 0;
         for (uint256 i = 0; i < num_epochs; i++) {
             uint256 total = epoch_totals[starting_epoch + i];
             if (total > 0) {
-                uint256 user = epochs[starting_epoch + i][sender];
-                epochs[i][sender] = 0;
-                mint_amount += rewardOf(starting_epoch + i) * user / total;
+                uint256 user = epochs[starting_epoch + i][msg.sender];
+                epochs[i][msg.sender] = 0;
+                mint_amount += (rewardOf(starting_epoch + i) * user) / total;
             }
         }
-        _mint(sender, mint_amount);
+        _mint(msg.sender, mint_amount);
     }
 }
