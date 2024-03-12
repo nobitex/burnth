8952cb3cba759649791d67dc934a534f209d7556
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2024-03-12 12:25:44 +0330

    [FEAT] Add ui command

--- commands/__init__.py
+++ commands/__init__.py
@@ -2,3 +2,4 @@ from commands.info import info_cmd, InfoContext
 from commands.burn import burn_cmd, BurnContext
 from commands.mint import mint_cmd, MintContext
 from commands.spend import spend_cmd, SpendContext
+from commands.ui import ui_cmd
