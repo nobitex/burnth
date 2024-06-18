26f3af4bdaa1fdf3656d419b50f544e3058d3ca4
toolabi <prds.toolabi@gmail.com>
2024-06-18 22:01:19 +0330

    Refactor(core): updating file paths.

--- test/Burnth.t.sol
+++ test/Burnth.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Burnth} from "../src/Burnth.sol";
+import {Burnth} from "../src/contracts/Burnth.sol";
 
 contract BurnthTest is Test {
     Burnth public burnth;
